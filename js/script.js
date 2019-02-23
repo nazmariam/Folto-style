87217d197dbe82ad06224607d769848936d3208d
nazmariam <fedirko.mary@gmail.com>
2019-02-23 17:33:14 +0200

    init and backbone

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$( document ).ready(function() {
+
+    var setClock = 600;
+
+    $('.clock').each(function() {
+        var clock = $(this);
+        clock.FlipClock(setClock, {
+            clockFace: 'HourlyCounter',
+            countdown: true,
+            language: 'it',
+            autoStart:true
+        });
+    });
+
+    $('.faq_list h3').click(function (e) {
+        $(this).next('.result').slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    $('select').niceSelect();
+
+    if (screen.width < 768) {
+        $('.slick').slick({
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+            arrows:false,
+            responsive: [
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        })
+    }
+    function resizeViseo() {
+        var video = $('#video_top');
+        var videoWidth = video.width();
+        var videoHeight = videoWidth*0.60;
+        video.css('height',videoHeight);
+    }
+    resizeViseo();
+
+    $( window ).resize(function() {
+        resizeViseo();
+    });
+
+
+    $('.mobile-menu').click(function () {
+        $('.page-menu').toggleClass('active');
+        $('body').css('overflow','hidden');
+        $('.mobile-menu').toggle();
+    });
+    $('.page-menu').click(function () {
+        $('.page-menu').toggleClass('active');
+        $('body').css('overflow','auto');
+        $('.mobile-menu').toggle();
+    });
+
+});
